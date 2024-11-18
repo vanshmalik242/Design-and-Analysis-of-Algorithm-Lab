8ebea739b0872be172ad03311502b06ec6a30d89
Vansh Malik <141725694+vanshmalik242@users.noreply.github.com>
2024-11-18 15:26:43 +0530

    Create q5.c

---
+++ EXPERIMENT-1/q5.c
@@ -0,0 +1,10 @@
+//Calculate the remainder, and the quotient of two given numbers.
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("Quotient: %d, Remainder: %d\n", a / b, a % b);
+    return 0;
+}
