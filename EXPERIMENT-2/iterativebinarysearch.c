0c9a723e6fc23ad14ccf439b7633abb46ba71edb
Vansh Malik <141725694+vanshmalik242@users.noreply.github.com>
2024-11-18 15:33:19 +0530

    Update iterativebinarysearch.c

--- EXPERIMENT-2/iterativebinarysearch.c
+++ EXPERIMENT-2/iterativebinarysearch.c
@@ -1,1 +1,26 @@
+// Iterative Binary Search
 
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    int result = binarySearch(arr, n, target);
+    printf("Element found at index: %d\n", result);
+    return 0;
+}
