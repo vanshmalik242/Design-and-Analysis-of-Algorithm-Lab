3cda88814b1c18d309ac85bd49a48f88dab75176
Vansh Malik <141725694+vanshmalik242@users.noreply.github.com>
2024-11-18 15:27:02 +0530

    Create q6.c

---
+++ EXPERIMENT-1/q6.c
@@ -0,0 +1,10 @@
+// Generate even numbers between 100 and 200.
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 100; i <= 200; i += 2)
+        printf("%d \n ", i);
+    printf("\n");
+    return 0;
+}
